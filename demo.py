bf3c92a5fbbf809c57e49cac79c466fd55dd4f3c
mahi <mahendran4266@gmail.com>
2025-07-20 20:56:18 +0530

    fourth commit

--- demo.py
+++ demo.py
@@ -4,3 +4,8 @@ class Hello:
     
     def farewell(self):
         return "Goodbye, World!"
+    
+if __name__ == "__main__":
+    hello = Hello()
+    print(hello.greet())
+    print(hello.farewell())

f209724c7b8591c5a777585bee449f5a55325bdd
mahi <mahendran4266@gmail.com>
2025-07-20 21:06:53 +0530

    seventh commit

--- demo.py
+++ demo.py
@@ -9,3 +9,6 @@ if __name__ == "__main__":
     hello = Hello()
     print(hello.greet())
     print(hello.farewell())
+
+# This is a simple demonstration of a class with greeting methods.
+# You can run this script to see the output of the greet and farewell methods.
